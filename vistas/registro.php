b7d65f2e41fe90fad2248ed7b3f648f43406ad1d
Ruben Abeledo <rubenas@wappingweb.com>
2025-05-22 13:26:18 +0200

    Solucionado error al añadir usuario nuevo

--- vistas/registro.php
+++ vistas/registro.php
@@ -22,22 +22,22 @@
         <?php unset($_SESSION['error']); ?>
       <?php endif; ?>
       <form action="../controladores/registrarUsuario.php" method="POST">
-        <label for="nombre">Nombre:</label>
+        <label for="nombre">Nombre*:</label>
         <input type="text" id="nombre" name="nombre" required placeholder="Como mínimo 2 caracteres.">
 
-        <label for="apellido">Apellidos:</label>
+        <label for="apellido">Apellidos*:</label>
         <input type="text" id="apellido" name="apellido" required>
 
-        <label for="correo">Correo electrónico:</label>
+        <label for="correo">Correo electrónico*:</label>
         <input type="text" id="correo" name="correo" required placeholder="CorreoEjemplo@gmail.com">
 
-        <label for="imagenPerfil">Enlace de foto de perfil:</label>
-        <input type="text" name="imagenPerfil" id="imagenPerfil" placeholder="https://...">
+        <label for="imagenPerfil">Enlace de foto de perfil*:</label>
+        <input type="text" name="imagenPerfil" id="imagenPerfil" placeholder="https://..." required>
 
-        <label for="password">Contraseña:</label>
+        <label for="password">Contraseña*:</label>
         <input type="password" id="password" name="password" required placeholder="Como mínimo: 1 Mayúscula, 8 caracteres, 1 Carácter especial.">
 
-        <label for="confirmar">Repetir contraseña:</label>
+        <label for="confirmar">Repetir contraseña*:</label>
         <input type="password" id="confirmar" name="confirmar" required placeholder="Repite la contraseña anterior.">
         <!--Utilizo un checkbox para mostrar la contraseña en formato texto -->
         <p><input type="checkbox" id="verPassword" onclick="verContrasena()"> Mostrar contraseñas</p>
